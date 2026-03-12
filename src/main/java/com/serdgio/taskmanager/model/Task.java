50ecdafcbfa45629b8a2ff083cb4856bdb431bbb
Serdgio1 <Suprun_777@list.ru>
2026-03-12 10:26:42 +0300

    Added correct idCounter

--- src/main/java/com/serdgio/taskmanager/model/Task.java
+++ src/main/java/com/serdgio/taskmanager/model/Task.java
@@ -4,9 +4,9 @@ import com.serdgio.taskmanager.service.TaskService;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
-
 public class Task {
-    private int id = 0;
+    private int id;
+    private static int idCounter = 0;
     private String title;
     private String description;
     TaskSatus satus;
@@ -15,7 +15,7 @@ public class Task {
     Priority priority;
 
     public Task(String title, String description, Instant createdAt, LocalDateTime deadline, Priority priority) {
-        this.id = ++id;
+        this.id = ++idCounter;
         this.title = title;
         this.description = description;
         this.createdAt = createdAt;
@@ -27,8 +27,55 @@ public class Task {
         return id;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public int getIdCounter() {
+        return idCounter;
+    }
+
+    public void setIdCounter(int idCounter) {
+        Task.idCounter = idCounter;
+    }
     public void setId(int id) {
         this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setDeadline(LocalDateTime deadline) {
+        this.deadline = deadline;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
     }
 
     @Override
